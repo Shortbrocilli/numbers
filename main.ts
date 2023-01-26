070ec05f4ba7aada1ae7e5d65326223ec89a33aa
Shortbrocilli <122543889+Shortbrocilli@users.noreply.github.com>
2023-01-26 09:38:50 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    Number2 += 1
+    basic.showNumber(Number2, 50)
+})
+input.onButtonPressed(Button.AB, function () {
+    Number2 = 0
+    basic.showNumber(Number2, 50)
+})
+input.onButtonPressed(Button.B, function () {
+    Number2 += -1
+    basic.showNumber(Number2, 50)
+})
+let Number2 = 0
+Number2 = 0
+basic.showNumber(Number2, 50)
+basic.forever(function () {
+	
+})
